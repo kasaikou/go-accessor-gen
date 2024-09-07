b07a1bfc99c41cb19ba71b8080a0bfaafb86df12
Kasai Kou <80010105+kasaikou@users.noreply.github.com>
2024-09-07 20:53:40 +0900

    fix label to tag

--- goacc/cmd/parse_generate_config.go
+++ goacc/cmd/parse_generate_config.go
@@ -8,12 +8,12 @@ import (
 
 func ParseGenerateFlag(args []string) *entity.GenerateConfig {
 	includePattern := flag.String("i", "**.go", "Search file pattern.")
-	defaultLabel := flag.String("l", "-", "Default label, uses cannot found 'goacc' label.")
+	defaultTag := flag.String("t", "-", "Default label, uses cannot found 'goacc' label.")
 
 	flag.CommandLine.Parse(args)
 
 	return entity.NewGenerateConfigBuilder(
 		*includePattern,
-		*defaultLabel,
+		*defaultTag,
 	).Purge()
 }
